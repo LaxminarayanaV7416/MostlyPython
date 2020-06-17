d267209e34e2b1c721a694a8e316c4fd9800e3d9
LaxminarayanaV7416 <laxminarayana.vadnala@gmail.com>
2020-06-17 07:46:35 +0530

    started looking out for material on oops and great content

---
+++ Python OOPS/basics_of_classes.py
@@ -0,0 +1,11 @@
+# classes are used to logically group data together
+# every class in python is inherited from object
+# function inside classes or associated with class are called as methods
+# varibles used inside classes which will be defined as self is known as variables
+# we will be defining classes and learning oops based on strict python syntax using typing package.
+
+import typing
+
+''' Most used python typing classes are 
+   List, Tuple, Dict, Set, int, float, str, Sequence, NewType, Mapping, Callable, Iterable,  
+'''
